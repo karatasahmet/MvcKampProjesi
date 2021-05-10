d790f2d8f664a41db5b01eeffb6ae49efc13a2d1
karatasahmet <ikmalpc@gmail.com>
2021-05-10 23:34:18 +0300

    MvcKampProjesi
    Murat Yücedağ ile C#-MvcKampProjesi

---
+++ MvcProjeKampi/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using DataAccesLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class CategoryManager
+    {
+        GenericRpository<Category> repo = new GenericRpository<Category>();
+        public List<Category> GetAllBL()
+            {
+            return repo.List();
+            }
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length >= 51)
+            {
+                //hata mesajı
+            }
+            {
+                repo.Insert(p);
+            }
+           
+        }
+    }
+}
---
+++ MvcProjeKampi/DataAccesLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Abstract
+{
+   public interface ICategoryDal:IRepository<Category>
+    {
+
+
+        ////CRUD
+        ////Listeleme
+        //List<Category> List();
+
+        ////Ekleme - değer döndermeyeceğinden viod kullanılır.
+        //void Insert(Category p);
+
+        ////Güncelleme
+        //void Update(Category p);
+
+        ////Silme
+        //void Delete(Category p);
+    }
+}
